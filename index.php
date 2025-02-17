0371df48efc78b8952e6b08bc6c843e017ca3e82
aashutoast <ashish.sharma546956@gmail.com>
2025-02-17 18:14:59 +0530

    index page making

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <title>Publiccccc</title>
+</head>
+<body class="bg-gray-0]0">
+  <div class="main">
+    <div id="global_padding" class="px-8">
+        <div id="global_max_width" class="mx-auto max-w-7xl">
+            <section id="hero_section">
+                <div class="py-20">
+                    <div class="grid grid-cols-2 gap-20">
+                        <div class="hiroes_content_s1 max-w-md">
+                            <div>
+                                <h1 class="text-4xl font-bold mb-2">Household and Lifestyle Services to Your Doorstep</h1>
+                                <p class="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc nec</p>
+                            </div>
+                            <button class="mt-10 bg-slate-900 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded">Get Started</button>
+                        </div>
+                        <div class="hiroes_content_s1 max-w-auto">hi</div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
