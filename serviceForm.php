c08befdaab3f0ed10639503524c2c3ccafb65419
aashutoast <ashish.sharma546956@gmail.com>
2025-02-18 15:57:08 +0530

    table added without modal

--- serviceForm.php
+++ serviceForm.php
@@ -43,44 +43,66 @@ if (!isset($_SESSION["admin_logged_in"]) || $_SESSION["admin_logged_in"] != true
 </head>
 
 <body>
-    <div class="container">
+    <div class="container-xxl">
         <div class="row justify-content-center">
-            <div class="col-md-6">
-                <a class="btn btn-primary w-100" href="../hiroes_Website/Config/logout.php">Logout</a>
-                <div class="form-container">
-                    <h4 class="text-start">Admin Service Form</h4>
-
-
-                    <form id="serviceForm">
-                        <div class="mb-3">
-                            <label for="category" class="form-label">Category</label>
-                            <select class="form-select" id="category" required>
-                                <option value="">Select Category</option>
-                                <option value="Plumbing">Air Conditioner and Heating</option>
-                                <option value="Electrical">Vehicle Maintenance</option>
-                                <option value="Carpentry">General Repair and Services</option>
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="description" class="form-label">Category Description</label>
-                            <textarea class="form-control" id="description" rows="3" required></textarea>
-                        </div>
-                        <div class="mb-3">
-                            <label for="serviceName" class="form-label">Service Name</label>
-                            <input type="text" class="form-control" id="serviceName" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="serviceIcon" class="form-label">Service Icon Name</label>
-                            <input type="text" class="form-control" id="serviceIcon" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="price" class="form-label">Service Price</label>
-                            <input type="text" class="form-control" id="price" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary w-100" id="submitBtn" disabled>Submit</button>
-
-                    </form>
-                </div>
+            <div class="col-md-8">
+                    <div class="form-container h-100">
+                        <h4 class="text-start pb-3">Service List</h4>
+                        <table class="table table-striped table-hover table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col" class="col-4">Name</th>
+                                    <th scope="col" class="col-4 d-none d-lg-block w-100">Category</th>
+                                    <th scope="col" class="col-2">Price</th>
+                                    <th scope="col" class="col-2">Status</th>
+                                    <!-- <th scope="col">Edit</th> -->
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">AC Deep Cleaning<a href="#" class="link-secondary px-2 text-decoration-none">📝</a></th>
+                                    <td class="col-4 d-none d-lg-block w-100">Air Conditioner & Heating</td>
+                                    <td>₹700</td>
+                                    <td>Modified</td>
+                                    <!-- <td><button class="btn btn-outline-primary btn-sm">Edit</button></td> -->
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+            </div>
+            <div class="col-md-4">
+                    <div class="form-container">
+                        <h4 class="text-start pb-3">Admin Service Form</h4>
+                        <form id="serviceForm">
+                            <div class="mb-3">
+                                <label for="category" class="form-label">Category</label>
+                                <select class="form-select" id="category" required>
+                                    <option value="">Select Category</option>
+                                    <option value="Plumbing">Air Conditioner and Heating</option>
+                                    <option value="Electrical">Vehicle Maintenance</option>
+                                    <option value="Carpentry">General Repair and Services</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="description" class="form-label">Category Description</label>
+                                <p id="description" class="fs-5 text">Just read only content</p>
+                            </div>
+                            <div class="mb-3">
+                                <label for="serviceName" class="form-label">Service Name</label>
+                                <input type="text" class="form-control" id="serviceName" required>
+                            </div>
+                            <!-- <div class="mb-3">
+                                <label for="serviceIcon" class="form-label">Service Icon Name</label>
+                                <input type="text" class="form-control" id="serviceIcon" required>
+                            </div> -->
+                            <div class="mb-3">
+                                <label for="price" class="form-label">Service Price</label>
+                                <input type="number" class="form-control" id="price" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary w-100" id="submitBtn" disabled>Submit</button>
+                            <a class="btn btn-light w-100 mt-2" href="../hiroes_Website/Config/logout.php">Logout</a>
+                        </form>
+                    </div>
             </div>
         </div>
     </div>
